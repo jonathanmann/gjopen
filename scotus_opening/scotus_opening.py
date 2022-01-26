6d2bd4f2a96a245b765ffe8b37972ed49798b1b5
Jonathan Mann <jonathan.mann@nyu.edu>
2022-01-25 22:37:22 -0500

    update scotus opening simulation

--- scotus_opening/scotus_opening.py
+++ scotus_opening/scotus_opening.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 import pandas as pd
+import datetime
+
+EXPIRATION = datetime.date(2023,1,4)
+REMAINING_DAYS = (EXPIRATION - datetime.date.today()).days
+DAYS_IN_YEAR = 365
+REMAINING_SCALAR = REMAINING_DAYS/DAYS_IN_YEAR
+BREYER_PLANNED_RETIREMENT_ESTIMATE = .3333
+
 mortality = dict(pd.read_csv('mortality.tsv',sep='\t'))
 mortality['F'] = dict(mortality['F'])
 mortality['M'] = dict(mortality['M'])
+
 justices = {
     'Stephen Breyer': ['M',83],
     'Clarence Thomas':['M',73],
@@ -15,10 +24,10 @@ justices = {
     'Amy Barrett':['F',49],
     }
 
-p = 1
+p = (1 - BREYER_PLANNED_RETIREMENT_ESTIMATE) # tracks the probability that no one leaves
+
 for j in justices:
     g,a = justices[j]
-    p = p * (1 - mortality[g][a])
+    p = p * (1 - mortality[g][a]) 
 
-print((1 - (1 - p) * 11.3/12))
-
+print(1 - (1 - p) * REMAINING_SCALAR)
