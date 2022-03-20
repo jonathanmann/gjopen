c9cdefdd6fe86b0d5128dd8a7f81a9359bba2de0
Jonathan Mann <jonathan.mann@nyu.edu>
2022-03-20 19:55:19 -0400

    add trading days to Monte Carlo simulator

--- meta/mc_meta.py
+++ meta/mc_meta.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('..')
 from monte_carlo_forecasting.MonteCarlo import Simulation as mc
 
-s = mc('FB','2022-05-20',0,238.0,214.70)
+s = mc('FB','2022-05-20',0,238.0,214.70,weekly_trading_days = 5)
 s.run_trials(sudden_condition=True)
 
 
